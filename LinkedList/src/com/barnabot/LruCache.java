796ea46e20eaa56256b82499ddad6e8e5422b65e
Henrique Barnabé <barnabot@gmail.com>
2016-09-01 04:27:28 -0300

    add lru

--- LinkedList/src/com/barnabot/LruCache.java
+++ LinkedList/src/com/barnabot/LruCache.java
@@ -3,13 +3,13 @@ package com.barnabot;
 import java.util.HashMap;
 
 /**
- * Created by hbarnabe on 30/08/2016.
+ * Created by hbarnabe on 02/08/2016.
  */
 public class LruCache {
     int capacity;
     HashMap<Integer, Node> Map = new HashMap<Integer, Node>();
     Node Head = null;
-    Node end = null;
+    Node End = null;
 
     public LruCache(int capacity)
     {
@@ -31,16 +31,62 @@ public class LruCache {
 
     public void Remove(Node n)
     {
+        if(null != n.pre)
+        {
+            n.pre.next = n.next;
+        }
+        else{
+            Head = n.next;
+        }
 
+        if(null != n.next)
+        {
+            n.next.pre = n.pre;
+        }
+        else{
+            End = n.pre;
+        }
     }
+
 
     public void SetHead(Node n)
     {
+        n.next = Head;
+        n.pre = null;
 
+        if(null != Head)
+        {
+            Head.pre = n;
+        }
+
+        Head = n;
+
+        if(null != End){
+            End = Head;
+        }
     }
 
     public void Set(int key, int value)
     {
+        if(Map.containsKey(key))
+        {
+            Node old = Map.get(key);
+            old.value = value;
+            Remove(old);
+            SetHead(old);
+        }
+        else{
+            Node created = new Node(key, value);
+            if(Map.size() >= capacity){
+                Map.remove(End.key);
+                Remove(End);
+                SetHead(created);
+            }
+            else{
+                SetHead(created);
+            }
 
+            Map.put(key, created);
+        }
     }
 }
